3ee5878883d62c5842dcfa434c0a3ceefcadeae5
clarkjoypesco@yahoo.com <clarkjoypesco@yahoo.com>
2019-04-15 07:51:31 +0800

    few changes

--- Anak.py
+++ Anak.py
@@ -3,6 +3,7 @@ import Magulang
 class Child(Magulang.Parent):
     
     def __init__(self,last_name,eye_color, number_of_toys):
+        print 'Child Constructor Called!'
         Magulang.Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
