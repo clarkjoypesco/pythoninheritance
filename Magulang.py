8111ae0460ace0b5ef17ed7ee2586d583089ce4d
clarkjoypesco@yahoo.com <clarkjoypesco@yahoo.com>
2019-04-15 08:40:46 +0800

    reusing methods

--- Magulang.py
+++ Magulang.py
@@ -4,3 +4,7 @@ class Parent():
         print 'Parent Constructor Called!'
         self.last_name = last_name
         self.eye_color = eye_color
+    
+    def show_info(self):
+        print ' Last Name - ' + self.last_name
+        print ' Eye Color - ' + self.eye_color
--- Person.py
+++ Person.py
@@ -1,5 +1,6 @@
 import Anak
 
 miley_cyrus = Anak.Child('Cyrus','blue',34)
-print miley_cyrus.last_name
-print miley_cyrus.number_of_toys
+#print miley_cyrus.last_name
+#print miley_cyrus.number_of_toys
+print miley_cyrus.show_info()
